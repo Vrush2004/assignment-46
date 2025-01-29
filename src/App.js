46a69b0aec375a0127b68dc3c5bac7abc64ac1d2
Vrush2004 <vrushalichaube@gmail.com>
2025-01-29 11:31:49 +0530

    configurable ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import "./App.css";
+import {WEBSITE_NAME, WEBSITE_DESC, WEBSITE_URL, BUTTON1, BUTTON2} from "./config.js"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+        <h1 className='text-5xl my-5 mx-10 font-bold'>{WEBSITE_NAME}</h1>
+        <p className='text-lg mx-10'>{WEBSITE_DESC}</p>
+
+        <img src={WEBSITE_URL} alt="" className='mx-auto h-[450px] mt-5 w-[1450px] object-cover'/>
+
+        <div className='flex justify-center'>
+            <button className='bg-lime-900 m-5 px-4 py-1 rounded-lg text-white text-xl'>{BUTTON1}</button>
+            <button className='bg-lime-900 m-5 px-4 py-1 rounded-lg text-white text-xl'>{BUTTON2}</button>
+        </div>
+    </div>
   )
 }
 
---
+++ src/config.js
@@ -0,0 +1,13 @@
+const WEBSITE_NAME = "Cool Cravings 🍨";
+const WEBSITE_DESC = `We’re thrilled to serve you the freshest, creamiest ice creams, refreshing sorbets, and delicious sundaes, all made with love and the finest ingredients. Whether you're craving classic favorites or excited to explore unique new flavors, we have something special just for you!At Frozen Treats, every scoop is crafted to bring you joy. Customize your dessert with a variety of toppings, sauces, and add-ons to make your experience even more delightful. Our cozy and fun atmosphere makes it the perfect spot to chill with friends, family, or simply enjoy a sweet moment for yourself.`;
+const WEBSITE_URL = "https://assets.telegraphindia.com/telegraph/2022/Mar/1648476591_whatsapp-image-2022-03-28-at-19-36-29.jpeg";
+const BUTTON1 = "Explore Products";
+const BUTTON2 = "Call Now";
+
+// const WEBSITE_NAME = "Frozen Treats🍧";
+// const WEBSITE_DESC = `Satisfy your sweet tooth at Cool Cravings, where every bite is a burst of happiness! We serve rich, creamy ice creams, refreshing sorbets, and indulgent sundaes in a variety of delicious flavors. Whether you're craving a classic scoop, a loaded sundae, or a custom milkshake, we’ve got the perfect treat to cool you down and lift you up!`;
+// const WEBSITE_URL = "https://www.ktchnrebel.com/wp-content/uploads/2022/08/Various_coloured_ice_cream_shutterstock_1678666684.jpg";
+// const BUTTON1 = "Buy Now";
+// const BUTTON2 = "Contact Us";
+
+export {WEBSITE_NAME, WEBSITE_DESC, WEBSITE_URL, BUTTON1, BUTTON2};
